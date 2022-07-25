5aab6cc6f8ac0c6baf15ebbfcc084ef160098368
happycozero <fermer141@yandex.ru>
2022-07-25 15:11:31 +0300

    Rebuild 2.0
    The mini-application with the game has been removed, but its functionality has been restored

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # PR08
-Запускать главный файл _main.py
+To start the program, open _main.py
+
+P.S
+
+To move to the next mini-project, close the current one
+
+URL on figma: https://www.figma.com/team_invite/redeem/EinHEGIZ6KB6R5xTxYU0wq
--- _main.py
+++ _main.py
@@ -1,17 +1,18 @@
 from tkinter import *
 from subprocess import call
 
-def Open():
-    call(["python", "downthe1.py"])
-    call(["python", "_Snake_2\snake2.py"])
-    call(["python", "youtube3.py"])
-    call(["python", "weather4.py"])
 
-root=Tk()
+def Open():
+    call(["python", "Snake_2\snake2.py"])
+    call(["python", "YouTube-Download_3\youtube3.py"])
+
+
+root = Tk()
+root.title("Two in one")
 root.geometry('300x300')
 frame = Frame(root)
 frame.pack(pady=30, padx=30)
-btn=Button(frame, text='Full Pack', width=50, height=50, command=Open)
+btn = Button(frame, text='Start', width=50, height=50, command=Open)
 btn.pack()
 
 root.mainloop()
